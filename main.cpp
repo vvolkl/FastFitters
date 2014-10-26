4d290654e99bdab1667dd5c03f7f5cd68082f352
Juan-Antonio <cyberjuen@gmail.com>
2014-10-26 00:39:56 +0000

    First draft

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "FastHelix.h"
+#include "FastLine.h"
+#include "FastCircle.h"
+#include "GlobalPoint.h"
+
+int main(void)
+{
+   FastHelix fit (GlobalPoint(), GlobalPoint(), GlobalPoint(),
+}
